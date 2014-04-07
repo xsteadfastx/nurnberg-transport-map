6756ae68c783cc2548ac27faa72b7529dd83094b
Marvin Steadfast <xsteadfastx@gmail.com>
2014-04-07 15:06:32 +0200

    added first version

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ nurnberg-transport-map
 ======================
 
 a map with nurnberg public transport departures.
+
+![screenshot](screenshot.jpg)
---
+++ app/map.py
@@ -0,0 +1,84 @@
+import os
+import time
+import threading
+import geojson
+import vagquery
+from random import uniform
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask.ext.socketio import SocketIO, emit
+
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = os.urandom(24)
+Bootstrap(app)
+socketio = SocketIO(app)
+
+
+def get_station_list():
+    '''getting station list by bruteforce'''
+    with open('station.lst', 'w') as f:
+        for i in range(100, 1000):
+            try:
+                if vagquery.DepartureQuery(i).query():
+                    f.write(str(i) + '\n')
+            except Exception:
+                pass
+
+
+def is_here(station):
+    '''check if there is some departing in 0 minutes'''
+    departures = vagquery.DepartureQuery(station).query()
+
+    for i in departures:
+        if i.departure_in_min == 0:
+            return True, i.trip_id, i.latitude, i.longitude, i.direction
+        else:
+            return False, i.trip_id, i.latitude, i.longitude, i.direction
+
+
+def create_geojson():
+    '''getting everything together and returns a geojson'''
+    if not os.path.exists('station.lst'):
+        get_station_list()
+
+    station_list = []
+    with open('station.lst') as f:
+        for i in f.readlines():
+            station_list.append(i.strip())
+
+    here_list = []
+    for i in station_list:
+        station = is_here(i)
+        if station[0] == True:
+            here_list.append(geojson.Feature(geometry=geojson.Point((station[3], station[2])), properties={'id': station[1], 'popupContent': '&rarr; ' + station[4]}))
+
+    return geojson.FeatureCollection(here_list)
+
+
+def emit_coords():
+    '''check every 60 seconds'''
+    while True:
+        time.sleep(60)
+        coords = create_geojson()
+        print coords
+        socketio.emit('my response', coords, namespace='/map')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('connect', namespace='/map')
+def map_connect():
+    print 'Client connected'
+    coords = create_geojson()
+    print coords
+    socketio.emit('my response', coords, namespace='/map')
+
+
+if __name__ == '__main__':
+    threading.Thread(target=emit_coords).start()
+    socketio.run(app)
